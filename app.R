5ede945524bfa5c118f067ee8b93b58946314e67
bellaboulter <118226794+bellaboulter@users.noreply.github.com>
2023-12-10 15:57:21 -0800

    Add files via upload
    TO DO: add information. I already put where to add it. It says "TO DO: ..."

--- app.R
+++ app.R
@@ -1,51 +1,47 @@
-# Aaron Fresco
-# 12-4-23
-# Average Temperature per
-# country over time
-
 library(dplyr)
 library(stringr)
 library(ggplot2)
 library(shiny)
+library(leaflet)
+
+bella_df <- read.csv("average_emmisions.csv")
+aaron_df <- read.csv("lat_lon.csv")
+dina_df <-read.csv("CombinedDataFrame.csv")
 
 about_view <- fluidPage(
   h1("Climate Change Analysis"),
-  h2("(Emissions, Temperatures, and Food Production/Consumption)"),
-  p("
-We have chosen to analyze data about climate change around the world. There are many different factors when it comes to climate change, but we have decided to mostly focus on countries’ emissions and rising temperatures. We are also curious how climate change relates to food production, so we have also analyzed data about several countries’ food production and food consumption.
-"),
+  h2("(Emissions, Temperatures, and Food Consumption)"),
+  p("We have chosen to analyze data about climate change around the world. There are many different factors when it comes to climate change, but we have decided to mostly focus on global emissions, rising temperatures, and food consumption."),
   p("We want our project to be up to interpretation, and we want to avoid making assumptions or drawing absolute conclusions, as we are fully aware that we have not examined every factor that contributes to the complicated topic of climate change. "),
-  p("However, our project has revealed interesting observations. 
-Our overall goal is to spark contemplation and conversation. 
-So as you explore our data, we ask:
-What patterns do you observe? What countries seem to be major outliers? How have emissions and temperatures changed in the last 30 years? How do food production and food consumption relate to each other and to climate change?
-"),
-  p("We hope this global climate change analysis was thought provoking and interesting! 
-")
+  p("However, our project has revealed interesting observations. Our overall goal is to spark contemplation and conversation."),
+  p("So as you explore our data, we ask:"),
+  p("What patterns do you observe? What countries seem to be major outliers? How have emissions and temperatures changed in the last 30 years? How do these subjects relate to each other and climate change as a whole?"),
+  p("We hope this global climate change analysis was thought provoking and interesting!")
 )
 
+# Dina's UI
 dina_page <- fluidPage(
-  titlePanel("Food Production vs Consumption"),
+  titlePanel("Exploring Food Consumption"),
   sidebarLayout(
     sidebarPanel(
       selectInput("Country", "Select Country:",
-                  choices = unique(CombinedDataFrame$Area)),
-      hr(),
-      helpText("Select a country to contrast food production and consumption.")
+                  choices = unique(dina_df$Area)),
+      p("TO DO: input information and questions"),
+      p("TO DO: input information and questions")
     ),
     mainPanel(
-      plotOutput("production_vs_consumption"),
-      tableOutput("summary_table")
+      plotOutput("food_consumption_plot"),
+      plotOutput("all_countries_food_consumption")
     )
   )
 )
 
-
-bella_analysis_view <- fluidPage(
+# Bella's UI
+bella_page <- fluidPage(
   titlePanel("Exploring Global Emissions"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("country", "Select Country:", choices = unique(average_emmisions$Area)),
+      selectInput("country", "Select Country:", choices = unique(bella_df$Area)),
       p("What countries seem to be striving for change?"),
       p("What could be the cause of dramatic drops or rises in emissions?")
     ),
@@ -72,39 +68,40 @@ bella_analysis_view <- fluidPage(
   )
 )
 
-# Main Ui Page
+# Aaron's UI
 aaron_page <- fluidPage(
   titlePanel("Average Temperature Over Years with Map"),
   
   mainPanel(
-    leafletOutput("country", width = '700px',)
+    leafletOutput("country", width = '700px')
   ),
   
   sidebarLayout(
     selectInput("country", "Select a Country:",
-                choices = unique(lat_lon$Area),
-                selected = unique(lat_lon$Area)[1]),
+                choices = unique(aaron_df$Area),
+                selected = unique(aaron_df$Area)[1]),
     plotOutput("temperaturePlot"),
   )
 )
 
-# Navbar of shiny app
+# Main UI
 ui <- navbarPage(
   "Climate Change Analysis (Emissions, Temperatures, and Food Production/Consumption)",
   tabPanel("About Our Project", about_view),
-  tabPanel("Global Emissions", bella_analysis_view),
-  tabPanel("Analysis View", aaron_page),
-  tabPanel("Food Production / Food Consumption", dina_page)
+  tabPanel("Global Emissions", bella_page),
+  tabPanel("Global Temperatures", aaron_page),
+  tabPanel("Global Food Consumption", dina_page)
 )
 
 server <- function(input, output) {
   
+  # Bella's server 
   filtered_data <- reactive({
-    filter(average_emmisions, Area %in% input$country)
+    filter(bella_df, Area %in% input$country)
   })
   
   output$all_countries <- renderPlot({
-    plot <- ggplot(average_emmisions, aes(x = Year, y = Avg_Emissions, color = Area, label = Area)) +
+    plot <- ggplot(bella_df, aes(x = Year, y = Avg_Emissions, color = Area, label = Area)) +
       geom_point(aes(color = Area)) +
       geom_line() +
       labs(title = "Average Emissions for All Countries 1990-2020",
@@ -170,10 +167,9 @@ server <- function(input, output) {
            y = "Average Emissions")
   }
   
-  
-  # Filter data only needed for temperature plot
+  # Aaron's server 
   filtered_aaron_data <- reactive({
-    filter(lat_lon, Area == input$country)
+    filter(aaron_df, Area == input$country)
   })
   
   output$country <- renderLeaflet({
@@ -188,10 +184,8 @@ server <- function(input, output) {
                        zoom = 5)
     
   })
-
   
-  # Plots average temperate of countries
-  output$temperaturePlot <- renderPlot({
+    output$temperaturePlot <- renderPlot({
     plot_data <- filtered_aaron_data()
     ggplot(plot_data, aes(x = Year, y = Average.Temperature..C)) +
       geom_line() +
@@ -199,31 +193,33 @@ server <- function(input, output) {
            x = "Year", y = "Temperature (°C)")
   })
   
-  selected_country_data <- reactive({
-    filter(CombinedDataFrame, Area == input$country)
+  # Dina's server 
+  filtered_data_dina <- reactive({
+      filter(dina_df, Area == input$Country)
   })
   
-  output$summary_table <- renderTable({
-    summarise(selected_country_data(),
-              Total_Production = sum(WH_2000 + RI_2000 + MZ_2000),
-              Total_Consumption = sum(Food.Household.Consumption))
-  })
-  
-  output$production_vs_consumption <- renderPlot({
-    dina_data <- selected_country_data()
-    total_prod <- dina_data$WH_2000[1] + dina_data$RI_2000[1] + dina_data$MZ_2000[1]
-    total_cons <- dina_data$Food.Household.Consumption[1]
-    
-    ggplot(dina_data, aes(x = Year)) +
-      geom_line(aes(y = total_prod, color = "blue", linetype = "solid")) +
-      geom_line(aes(y = total_cons, color = "red", linetype = "dashed")) + 
-      labs(title = paste("Food Production vs Consumption in", input$country),
+  output$food_consumption_plot <- renderPlot({
+    ggplot(filtered_data_dina(), aes(x = Year)) +
+      geom_line(aes(y = Food.Household.Consumption, color = "Food Consumption")) +
+      labs(title = "Food Consumption Over Time",
+           y = "Values",
            x = "Year",
-           y = "Amount")
+           color = "Legend") 
   })
   
+  countries_to_plot <- c("Afghanistan", "Brazil", "China", "France", "Germany", "India", "Japan", "Mexico", "Russia", "Malaysia",
+                         "Chile", "Canada", "Australia", "Argentina", "Indonesia", "Spain", "Saudi Arabia", "Turkey")
+  
+  countries_to_plot_data <- filter(dina_df, `Area` %in% countries_to_plot)
+  
+  output$all_countries_food_consumption <- renderPlot({
+    ggplot(countries_to_plot_data, aes(x = Year, y = Food.Household.Consumption, color = Area, label = Area)) +
+      geom_point(aes(color = Area)) +
+      geom_line() +
+      labs(title = "Food Consumption for All Countries 1990-2020",
+           x = "Decade",
+           y = "Household Food Conspumtion")
+  })
 }
 
 shinyApp(ui = ui, server = server)
-
-
